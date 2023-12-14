957e7cc91a8f438593c1f44d6214eadcd4936d05
GKastet <127600582+GKastet@users.noreply.github.com>
2023-12-14 22:43:30 +0100

    add: pages, 404, folders initial in components, folder images

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, lazy } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Loader from "./components/Loader/Loader";
+
+const Home = lazy(() => import("./pages/1.Home/Home"));
+const About = lazy(() => import("./pages/2.About/About"));
 
 function App() {
-  const [count, setCount] = useState(0)
-  // console.log('count: ', count);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return (    
+    <Suspense fallback={<Loader />}>
+       <Routes>
+         <Route path="/" element={<Layout />}>
+           <Route index element={<Home />} />
+           <Route path="about" element={<About />} />
+           <Route path="*" element={<Navigate to="/" />}></Route>
+         </Route>
+       </Routes>
+     </Suspense>
+  );
 }
 
-export default App
+export default App;
