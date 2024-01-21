bbdaa18de9c21d3f8e78727d81eb641d9aa33d22
Sankerp <sankerp78@gmail.com>
2024-01-21 21:09:14 +0200

    edit Styles add Fonts

--- src/helpers/textsAll/sectionFooter.js
+++ src/helpers/textsAll/sectionFooter.js
@@ -1,8 +1,14 @@
 export const textsFooter = {
     sitemap: {
         en: ["main", "about", "team", "services", "projects", "contacts"],
-        sk: ["sk0", "sk1", "sk2", "sk3", "sk4", "sk5"],
-        pl: ["pl0", "pl1", "pl2", "pl3", "pl4", "pl5"],
-        ua: ["головне", "про нас", "команда", "послуги", "проекти", "контакти"]
+        sk: ["hlavný", "o nás", "tím", "služby", "projektov", "kontakty"],
+        pl: ["główny", "o nas", "zespół", "usługi", "projektowanie", "kontakty"],
+        ua: ["головна", "про нас", "команда", "послуги", "проекти", "контакти"]
+    },
+    socials: {
+        en: ["Whatsapp", "Telegram"],
+        sk: ["Whatsapp", "Telegram"],
+        pl: ["Whatsapp", "Telegram"],
+        ua: ["Whatsapp", "Telegram"]
     }
 }
