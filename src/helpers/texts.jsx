35647ca9ad74764db433b1f8cef278f2ebfd64d4
GKastet <127600582+GKastet@users.noreply.github.com>
2024-01-22 09:34:18 +0100

    Update texts.jsx

--- src/helpers/texts.jsx
+++ src/helpers/texts.jsx
@@ -8,39 +8,39 @@ export const texts = {
 
 // export const textsHome = {};
 
-// export const textsAbout = {
-//   sectionIntroduction: {
-//     mainText: {
-//       eng: "English",
-//       sk: "Slovenský",
-//       pl: "Polski",
-//       ua: "Українська",
-//     },
-//   },
-//   sectionWhereIdeaMeets: {
-//     title: {
-//       eng: "Where Idea Meets Innovation in Every Line of Code",
-//       sk: "Kde sa nápad stretáva s inováciou v každom riadku kódu",
-//       pl: "Gdzie pomysł spotyka się z innowacją w każdym wierszu kodu",
-//       ua: "Де ідея та інновації зустрічаються у кожному рядку коду",
-//     },
-//     box1: {
-//       eng: "",
-//       sk: "",
-//       pl: "",
-//       ua: "",
-//     },
-//     box2: {
-//       eng: "",
-//       sk: "",
-//       pl: "",
-//       ua: "",
-//     },
-//     box3: {
-//       eng: "100% guaranteed result",
-//       sk: "100% garantovaný výsledok",
-//       pl: "100% gwarantowany wynik",
-//       ua: "100% гарантований результат",
-//     },
-//   },
-// };
+export const textsAbout = {
+  sectionIntroduction: {
+    mainText: {
+      eng: "English",
+      sk: "Slovenský",
+      pl: "Polski",
+      ua: "Українська",
+    },
+  },
+  sectionWhereIdeaMeets: {
+    title: {
+      eng: "Where Idea Meets Innovation in Every Line of Code",
+      sk: "Kde sa nápad stretáva s inováciou v každom riadku kódu",
+      pl: "Gdzie pomysł spotyka się z innowacją w każdym wierszu kodu",
+      ua: "Де ідея та інновації зустрічаються у кожному рядку коду",
+    },
+    box1: {
+      eng: "",
+      sk: "",
+      pl: "",
+      ua: "",
+    },
+    box2: {
+      eng: "",
+      sk: "",
+      pl: "",
+      ua: "",
+    },
+    box3: {
+      eng: "100% guaranteed result",
+      sk: "100% garantovaný výsledok",
+      pl: "100% gwarantowany wynik",
+      ua: "100% гарантований результат",
+    },
+  },
+};
